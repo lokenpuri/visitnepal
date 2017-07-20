740ae6c4c1628d11c64333517c0b931b6faabb38
Lokendra Puri <loken.puri@hotmail.com>
2017-07-20 16:55:12 +0545

    A sixth semester web tecnhnology project
    It is a web project developed using HTML, CSS, JS, PHP and MySqli.

---
+++ index.php
@@ -0,0 +1,234 @@
+<?php
+
+include'auth.php';
+include'layout/header.php';
+include'layout/carousel.php'; 
+
+?>
+
+<div id="pokhara" class="container-fluid">
+	<div class="jumbotron">
+		<h4 style="text-align:center">Places to visit in Nepal</h4>
+			<h4>POKHARA</h4>
+			<div class="row">
+				
+				
+					<p class="pokhara">Pokhara is Nepal's number 1 adventure and leisure city, 
+			           a trekking gateway to the 
+					Annapurna's with plenty of entertainment 
+						for individual travellers as well as families with kids. 
+					In Pokhara you can experience the best in trekking, boating, hiking, pony rides, paragliding or simply relax at one of the lakes near the city with the stunning Annapurna mountain range at the background.
+				
+					</p>  
+	`				<p class="pokhara">Pokhara will overwhelm you with one of the greatest photo spots in the world. 
+					While you walk along the shores of the Fewa lake you will be surrounded by a panoramical view over  the Himalayan peaks of the Annapurna Massif that reflect in this lake. A beautiful color combination that is created by the wooden boats and paragliders that are sailing, flying and rowing over the lake is probably going to be one of the highlights of your travel experience in Nepal. 
+					</p>	
+				
+					<p class="pokhara">Pokhara is well-known as a starting point for various trekking trail and expeditions in the Annapurna . Most of the hikers and trekkers for Annapurna and Ghandruk, respectively, build Pokhara as their first stop, or as relaxing station before they head out for serious walking. So, one may enjoy boating and reading books observing the serenity of the nature, or enjoy a couple of drinks, either in local restaurants or at a blues bar, or may simply enjoy sightseeing or cycling around the city. 
+					</p>
+				
+			</div>	
+			
+			<div class="row">
+				<h4 style="text-align: center">Some photos that represent Pokhara rich in natural beauties.</h4>
+			</div>
+	
+			<div class="row">
+				<div class="col-sm-4">
+					<img class ="pkr" src="images/p1.jpg">
+				
+				</div>
+		
+				<div class="col-sm-4">
+					<img class ="pkr" src="images/p11.jpg">
+					
+				</div>
+		
+				<div class="col-sm-4">
+					<img class ="pkr" src="images/p4.jpg">
+					
+				</div>
+			</div>
+			
+			<br>
+			<br>
+			
+			<div class="row">
+				<div class="col-sm-4">
+					
+					<img class ="pkr" src="images/p3.jpg">
+				</div>
+		
+				<div class="col-sm-4">
+				
+					<img class ="pkr" src="images/p8.jpg">
+				</div>
+		
+				<div class="col-sm-4">
+		
+					<img class ="pkr" src="images/p5.jpg">
+				</div>
+			</div>
+			
+			
+	</div>
+</div>	
+ <br>
+<div id="mustang" class="container-fluid">
+	<div class="jumbotron">
+		<h4>MUSTANG</h4>
+		<div class="row">	
+				<p class="pokhara">Mustang (from the Tibetan möntang (Wylie: smon-thang), Nepali: मुस्तांग "fertile plain"), formerly Kingdom of Lo, is a remote and isolated region of the Nepalese Himalayas. The Upper Mustang was a restricted demilitarized area until 1992 which makes it one of the most preserved regions in the world, with a majority of the population still speaking traditional Tibetic languages. Tibetan culture has been preserved by the relative isolation of the region from the outside world.
+				</p>  
+	`			<p class="pokhara">The Upper Mustang comprise the northern two-thirds of Mustang District of Dhawalagiri Zone, Nepal. The southern third of the district is called Thak and is the homeland of the Thakali, who speak the Thakali language, and whose culture combines Tibetan and Nepalese elements. Life in Mustang revolves around tourism, animal husbandry and trade.
+				</p>	
+				
+				<p class="pokhara">To trek in Upper Mustang is a rare privilege. Here you will experience the way of life of true mountain people, who were not much in touch with the rest of Nepal for hundreds of years, and even until recent times had an officially recognized king. In many ways, a trek into Upper Mustang is similar to trekking in Tibet, as geographically it is a part of the Tibetan plateau. The district of Mustang was, until 1950, a separate kingdom within the boundaries of Nepal. The last king, the Raja of Mustang, still has his home in the ancient capital known as Lo Manthang. 
+				</p>
+		
+		</div>	
+		<div class="row">
+			<h4 style="text-align: center">Some photos that represent Mustang rich in natural beauties.</h4>
+		</div>
+	
+		<div class="row">
+			<div class="col-sm-4">
+				<img class ="pkr" src="images/m1.jpg">
+				
+			</div>
+		
+			<div class="col-sm-4">
+				<img class ="pkr" src="images/m3.jpg">
+				
+			</div>
+		
+			<div class="col-sm-4">
+				<img class ="pkr" src="images/m5.jpg">
+				
+			</div>
+		</div>
+		<br>
+		<br>
+		<div class="row">
+			<div class="col-sm-4">
+				
+				<img class ="pkr" src="images/m2.jpg">
+			</div>
+		
+			<div class="col-sm-4">
+			
+				<img class ="pkr" src="images/m4.jpg">
+			</div>
+		
+			<div class="col-sm-4">
+				
+				<img class ="pkr" src="images/m6.jpg">
+			</div>
+		</div>	
+
+
+	</div>
+</div>	
+ 
+
+	
+<div id="chandragiri" class="container-fluid">
+	<div class="jumbotron">
+		<h4>CHANDRAGIRI</h4>
+			<div class="row">
+				<p class="pokhara">
+				Chandragiri Hills Ltd is a Hospitality Company which operates Cable Car, Restaurants, Amusement Park, Resort, Multipurpose hall and Branded Shops in Thankot Kathmandu, Nepal.
+			
+				</p>
+				<p class="pokhara">Chandragiri Hill, seven kilometres from Thankot, lies on the South-West of Kathmandu Valley and is 2551 metres from sea level. Soaring peaks and breath taking views of whole Kathmandu Valley covered with lush green blanket of flora makes Chandragiri truly a great bountiful and pristine landscape. The panoramic views of Kathmandu Valley & Himalayan ranges from Annapurna to Everest makes any one feel like seeing whole Nepal from this hill.
+				</p>  
+	`		
+				<p class="pokhara">The distance between Kathmandu and Thankot is 16 kilometres with approximately 31 minutes of travelling time. The single length of cable car project is around two and a half kilometres from near Godam at Thankot with the travelling time of 9 minutes. People willing to drive would have to pass through seven kilometres of rough road to reach the hill top. Another option for the visitors to reach the top is three hour long trek.
+				</p>
+			</div>
+			
+			
+			<div class="row">
+				<h4 style="text-align: center">Some of our photos clicked at Chandragiri.</h4>
+			</div>
+	
+			<div class="row">
+				<div class="col-sm-4">
+					<img class ="pkr" src="images/c9.jpg">
+					
+				</div>
+		
+				<div class="col-sm-4">
+					<img class ="pkr" src="images/c5.jpg">
+					
+				</div>
+		
+				<div class="col-sm-4">
+					<img class ="pkr" src="images/c7.jpg">
+					
+				</div>
+			</div>
+			
+			<br>
+			<br>
+			
+			<div class="row">
+				<div class="col-sm-4">
+					
+					<img class ="pkr" src="images/c4.jpg">
+				</div>
+		
+				<div class="col-sm-4">
+					
+					<img class ="pkr" src="images/c6.jpg">
+				</div>
+		
+				<div class="col-sm-4">
+					
+					<img class ="pkr" src="images/c12.jpg">
+				</div>
+			</div>
+		
+	</div>
+</div>		
+		
+		
+		
+	<div class="container-fluid">	
+		<div class="jumbotron">
+		
+			<div class = "row">
+				<p>There are other many places in Nepal that represent Nepal rich in natural beauties, historical, 
+				religious and cultural activities. Places such as Lumbini, Pashupatinath Temple, Swoyambhu, Royal Chitwan National Park and other National parks, Conservation areas also play important role in increasing national income of the country by the help of their immense beauties and importances. Being a Nepali, we should visit different parts of our country. 
+				Also, we should go to different places where people haven't reached. We should make our country glow in the world. </p>
+			</div>
+		
+		</div>
+	</div>	
+
+		<?php include'layout/footer.php' ?>
+	<!-- <div id = "contact" class="container-fluid">
+		<div class="jumbotron">  
+			<div class ="row">
+				<h2>Contact Us</h2>
+			</div>
+			<div class="footer">
+				<div class ="row">
+					<p>Made by <a href="https://www.facebook.com/loken.puri"target="_blank">Lokendra Puri</a></p> 
+				
+				</div>	
+				<div class="row">
+					
+							<a href="https://twitter.com/SozjoKeto" target="_blank"><button type="button" class="btn btn-primary">Twitter</button><a/>
+							<a href="https://www.facebook.com/loken.puri" target="_blank"><button type="button" class="btn btn-primary">Facebook</button></a>
+							<a href ="https://plus.google.com/u/0/" target="_blank"><button type="button" class="btn btn-primary">Google+</button></a>
+                </div>
+			</div>
+          <div class="row">
+           <h3 style="text-align: center">© 2017. All rights reserved</h3>
+          </div>
+		  
+		</div> -->
+	</div>
+</body>
+</html>
