377eaaa26f95eb0fb5029844e62c9adeba5f741a
Lokendra Puri <loken.puri@hotmail.com>
2018-06-07 00:02:12 +0545

    Add files via upload

---
+++ visitnepal/layout/footer.php
@@ -0,0 +1,27 @@
+<div id = "contact" class="container-fluid">
+	<div class="jumbotron">  
+		<div class ="row">
+			<h2>Contact Me</h2>
+		</div>
+
+		<div class="footer">
+			<div class ="row">
+				<p>Made by <a href="profile.php?p=profile">Lokendra Puri</a></p> 				
+			</div>	
+		
+			<div class="row">
+					
+				<a href="https://twitter.com/SozjoKeto" target="_blank"><button type="button" class="btn btn-primary">Twitter</button><a/>
+				<a href="https://www.facebook.com/loken.puri" target="_blank"><button type="button" class="btn btn-primary">Facebook</button></a>						
+				<a href ="https://plus.google.com/u/0/111256678037439617960" target="_blank"><button type="button" class="btn btn-primary">Google+</button></a>
+
+            </div>
+		</div>
+          
+        <div class="row">
+           <p style="text-align: center">© 2017. All rights reserved</p>
+        </div>
+		  
+	</div>
+	
+</div>
---
+++ visitnepal/layout/header.php
@@ -0,0 +1,74 @@
+<?php 
+  
+ ?>
+
+<!DOCTYPE html>
+<html >
+<head>
+<title>Visit Nepal</title>
+
+
+	<meta charset="UTF-8">
+	 
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="assets/main.css">
+	
+	<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+       <a class="navbar-brand" href="index.php">Visit Nepal</a>
+    </div>
+
+
+
+   <!--  <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">Visit Nepal</a>
+    </div> -->
+     <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+         <li class="active"><a href="index.php">Home</a></li>
+	  
+	        <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#">Places
+              <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#pokhara">Pokhara</a></li>
+  		        <li><a href="#mustang">Mustang</a></li>
+  			      <li><a href="#chandragiri">Chandragiri</a></li>
+            </ul>
+         </li>
+	    <li><a href="#contact">Contact Me</a></li>
+     </ul>
+     
+     <ul class="nav navbar-nav navbar-right">
+      
+      <?php if(isset($_SESSION['username'])) {?>
+     <li><a href="profile.php?p=profile">Profile</a></li>
+      <li><a href="login.php?p=logout">Logout</a></li>
+    <?php } else { ?>
+      <li><a href="login.php?p=login">Login</a></li>
+    <?php } ?>
+
+    </ul>
+  </div>
+  </div>
+</nav>
+</head>
+<body>
+
+
+
+</div>
+
+</head>
